3208b6455b8b548c7e8722317542b0cbfcf5d4dd
Seth B. Altshuler <sethb78@gmail.com>
2014-02-11 12:57:50 -0500

    lightened carousel

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
     before_filter :carousel_variables
+    before_filter :globals
 
   protect_from_forgery with: :exception
 
@@ -11,3 +12,7 @@ end
 def carousel_variables
     @carousel_slides = Carousel.all.order("priority")
 end
+
+def globals
+   @message = Message.new
+  end
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -7,7 +7,7 @@ class MessagesController < ApplicationController
     @message = Message.new(params[:message])
     if @message.valid?
       ContactMailer.contact_message(@message).deliver
-      redirect_to contact_path, notice: "Message sent! Thank you for contacting us, we will respond shortly."
+      redirect_to :back, notice: "Message sent! Thank you for contacting us, we will respond shortly."
     else
       render "new"
     end
