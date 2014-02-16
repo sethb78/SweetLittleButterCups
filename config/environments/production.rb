ce802a2bc4619a5e763225f2469c526e0721c5b6
Seth B. Altshuler <sethb78@gmail.com>
2014-02-15 19:08:12 -0500

    update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,8 +27,9 @@ Kristine::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
-
+config.serve_static_assets = true
+config.assets.compile = true
+config.assets.digest = true
   # Generate digests for assets URLs.
   config.assets.digest = true
 
