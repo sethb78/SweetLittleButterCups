e57b08118e9c3879b5f65399ddff073054601e72
Seth B. Altshuler <sethb78@gmail.com>
2014-02-15 19:01:56 -0500

    update

--- config/application.rb
+++ config/application.rb
@@ -25,5 +25,6 @@ module Kristine
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
+config.assets.initialize_on_precompile = true
   end
 end
